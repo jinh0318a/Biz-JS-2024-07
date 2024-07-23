a0187af79c1e5900d657f41eabaca6459a16546e
jinh0318a <jinh0318a@naver.com>
2024-07-23 09:16:34 +0900

    first

---
+++ hello-001/naver.js
@@ -0,0 +1,9 @@
+const naver = () => {
+  fetch("http://localhost:8080/student")
+    .then((res) => {
+      return res.text();
+    })
+    .then((text) => console.log(text));
+};
+
+export { naver };
